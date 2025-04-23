7540e064cd164ea5de9e0a76d8f0487f82319898
usamadevv <85132312+usamadevv@users.noreply.github.com>
2025-04-23 23:32:13 +0500

    Update diet.router.js

--- src/routes/diet.router.js
+++ src/routes/diet.router.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { createDietPlan, findDietPlan } = require('./diet.controller');
+const { createDietPlan, findDietPlan, getAll } = require('./diet.controller');
 const { verifyToken, verifyTokenAndGetUser } = require('../utils/token');
 const dietRouter = express.Router();
 
 // POST endpoint to create diet plan
 dietRouter.post('/api/diet/create', createDietPlan);
 
-
-dietRouter.post('/api/diet/find',verifyTokenAndGetUser,findDietPlan);
+dietRouter.get('/api/diet/getall', getAll);
+dietRouter.post('/api/diet/find',findDietPlan);
 
 module.exports = dietRouter;
